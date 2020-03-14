2dc414f60be5f8aa7a4fda0e1229c55bc1eb8fcf
saiesaire <rozenkreuz1126@gmail.com>
2020-03-14 16:18:55 +0900

    Update stars.js

--- app/assets/javascripts/stars.js
+++ app/assets/javascripts/stars.js
@@ -3,13 +3,13 @@ $(function(){
     var color_list = ["red","blue","green","yellow","purple","white","pink","orange"];
     var color_number = Math.floor(Math.random() * Math.floor(7));
     var color = color_list[color_number];
-    var top  = Math.ceil( Math.random()*70 );
-    var left = Math.ceil( Math.random()*100 );
-    var size = Math.ceil( Math.random()*150 );
-    let class = `
+    var top   = Math.ceil( Math.random()*70 );
+    var left  = Math.ceil( Math.random()*100 );
+    var size  = Math.ceil( Math.random()*150 );
+    let star = `
     <div class = "star" style = "background-color: ${color}; top: ${top}%; left: ${left}%; height: ${size}px; width: ${size}px;"></div>
-    `
-    $(".stars").append(class);
+    `;
+    $(".stars").append(star);
   });
   $(document).on("click",".ground__delete",function(){
     $(".stars").children().remove().fadeOut(700);
