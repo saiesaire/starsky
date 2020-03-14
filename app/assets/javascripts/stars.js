8d82de1bf7589626ffa502bc98fad62ff29fb289
saiesaire <rozenkreuz1126@gmail.com>
2020-03-14 16:06:43 +0900

    fix

--- app/assets/javascripts/stars.js
+++ app/assets/javascripts/stars.js
@@ -1,11 +1,11 @@
 $(function(){
   $(document).on("click",".ground__btn",function(){
     var color_list = ["red","blue","green","yellow","purple","white","pink","orange"];
-    var color_number = Math.ceil( Math.random()*9 );
+    var color_number = Math.floor(Math.random() * Math.floor(7));
     var color = color_list[color_number];
     var top  = Math.ceil( Math.random()*70 );
     var left = Math.ceil( Math.random()*100 );
-    var size = Math.ceil( Math.random()*100 );
+    var size = Math.ceil( Math.random()*150 );
     let star = `
     <div class = "star" style = "background-color: ${color}; top: ${top}%; left: ${left}%; height: ${size}px; width: ${size}px;"></div>
     `
